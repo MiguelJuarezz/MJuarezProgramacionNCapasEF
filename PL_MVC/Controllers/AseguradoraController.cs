f0f6409435f8f4acc291fb2bcd762933ebf61f90
MiguelJuarezz <jomiguelpejuarez@gmail.com>
2022-12-09 13:43:40 -0600

    SERVICIOS EN MVC

--- PL_MVC/Controllers/AseguradoraController.cs
+++ PL_MVC/Controllers/AseguradoraController.cs
@@ -13,26 +13,36 @@ namespace PL_MVC.Controllers
         [HttpGet] 
         public ActionResult GetAll()
         {
-            ML.Result result = new ML.Result();
-            result = BL.Aseguradora.GetAllEF();
+            ML.Aseguradora aseguradora = new ML.Aseguradora();
+            ServiceAseguradora.AseguradoraClient context = new ServiceAseguradora.AseguradoraClient();
+
+            var result = context.GetAll();
+            //ML.Result result = new ML.Result();
+            //result = BL.Aseguradora.GetAllEF();
 
             if (result.Correct)
             {
-                ML.Aseguradora aseguradora = new ML.Aseguradora();
-                aseguradora.Aseguradoras = result.Objects;
-                return View(aseguradora);
+                //ML.Aseguradora aseguradora = new ML.Aseguradora();
+                aseguradora.Aseguradoras = result.Objects.ToList();
+                //return View(aseguradora);
             }
             else
             {
                 ViewBag.Message = "Ocurrio un error al realizar la consulta";
-                return View();
+                //return View();
             }
+            return View(aseguradora);
         }
 
         [HttpGet]
         public ActionResult Form(int? IdAseguradora)
         {
             ML.Aseguradora aseguradora = new ML.Aseguradora();
+            ServiceAseguradora.AseguradoraClient context = new ServiceAseguradora.AseguradoraClient();
+
+
+
+            //ML.Aseguradora aseguradora = new ML.Aseguradora();
             aseguradora.Usuario= new ML.Usuario();
             aseguradora.Usuario.Rol = new ML.Rol();
 
@@ -42,12 +52,13 @@ namespace PL_MVC.Controllers
             if (IdAseguradora == null)
             {
                 aseguradora.Usuario.Usuarios = resultUsuario.Objects;
-                return View(aseguradora);
+                //return View(aseguradora);
             }
             else
             {
                 //GetbyId
-                ML.Result result = BL.Aseguradora.GetByIdEF(IdAseguradora.Value);
+                //ML.Result result = BL.Aseguradora.GetByIdEF(IdAseguradora.Value);
+                var result = context.GetById(IdAseguradora.Value);
 
                 if (result.Correct)
                 {
@@ -58,66 +69,80 @@ namespace PL_MVC.Controllers
                 {
                     ViewBag.Message = "Ocurrio un error al consultar la aseguradora seleccionada";
                 }
-                return View(aseguradora);
+                //return View(aseguradora);
             }
+            return View(aseguradora);
 
         }
 
         [HttpPost]
         public ActionResult Form(ML.Aseguradora aseguradora)
         {
-            ML.Result result = new ML.Result();
+
+            ServiceAseguradora.AseguradoraClient context = new ServiceAseguradora.AseguradoraClient();
+
+            
+            //ML.Result result = new ML.Result();
 
             if (aseguradora.IdAseguradora == 0)
             {
                 //add
-                result = BL.Aseguradora.AddEF(aseguradora);
+                //result = BL.Aseguradora.AddEF(aseguradora);
+                var result = context.Add(aseguradora);
 
                 if (result.Correct)
                 {
                     ViewBag.Message = "Se ha registrado la aseguradora";
-                    return PartialView("Modal");
+                    //return PartialView("Modal");
                 }
                 else
                 {
                     ViewBag.Message = "No se ha registrado la aseguradora" + result.ErrorMessage;
-                    return PartialView("Modal");
+                    //return PartialView("Modal");
                 }
+                return PartialView("Modal");
             }
             else
             {
                 //update
-                result = BL.Aseguradora.UpdateEF(aseguradora);
+                //result = BL.Aseguradora.UpdateEF(aseguradora);
+                var result = context.Update(aseguradora);
                 if (result.Correct)
                 {
                     ViewBag.Message = "Se ha actualizado la aseguradora";
-                    return PartialView("Modal");
+                    //return PartialView("Modal");
                 }
                 else
                 {
                     ViewBag.Message = "No se ha actualizado la aseguradora" + result.ErrorMessage;
-                    return PartialView("Modal");
+                    //return PartialView("Modal");
                 }
-
+                return PartialView("Modal");
             }
         }
 
         [HttpGet]
         public ActionResult Delete(int IdAseguradora)
         {
-            ML.Result result = new ML.Result();
-            result = BL.Aseguradora.DeleteEF(IdAseguradora);
+            ML.Aseguradora aseguradora = new ML.Aseguradora();
+            ServiceAseguradora.AseguradoraClient context = new ServiceAseguradora.AseguradoraClient();
+
+            var result = context.Delete(IdAseguradora);
+
+            //ML.Result result = new ML.Result();
+            //result = BL.Aseguradora.DeleteEF(IdAseguradora);
 
             if (result.Correct)
             {
                 ViewBag.Message = "Se ha elimnado el registro";
-                return PartialView("Modal");
+                //return PartialView("Modal");
             }
             else
             {
                 ViewBag.Message = "No se ha elimnado el registro" + result.ErrorMessage;
-                return PartialView("Modal");
+                //return PartialView("Modal");
             }
+            return PartialView("Modal");
         }
     }
 }
